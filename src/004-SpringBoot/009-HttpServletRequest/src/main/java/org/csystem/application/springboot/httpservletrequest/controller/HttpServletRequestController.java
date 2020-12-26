e0afa0cd23d1a868e6151eeb149fb639dbcba2a2
Oğuz Karan <oguzkaran@csystem.org>
2020-12-26 20:47:05 +0300

    JavaApp1-Jul-2020

--- src/004-SpringBoot/009-HttpServletRequest/src/main/java/org/csystem/application/springboot/httpservletrequest/controller/HttpServletRequestController.java
+++
@@ -1,8 +0,0 @@
-package org.csystem.application.springboot.httpservletrequest.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HttpServletRequestController {
-    //...
-}
